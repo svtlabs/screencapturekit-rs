c5651f9a3f73800f7f1821d5e2adba4ea9d588e7
Charles Zhang <aiz.cutei@gmail.com>
2023-12-17 21:35:02 +0800

    feat/add audio support and example for screencapturekit

--- screencapturekit/src/cm_sample_buffer.rs
+++ screencapturekit/src/cm_sample_buffer.rs
@@ -1,6 +1,6 @@
 use screencapturekit_sys::{
-    cm_sample_buffer_ref::CMSampleBufferRef, cv_image_buffer_ref::CVImageBufferRef,
-    os_types::rc::Id, sc_stream_frame_info::SCFrameStatus,
+    audio_buffer::CopiedAudioBuffer, cm_sample_buffer_ref::CMSampleBufferRef,
+    cv_image_buffer_ref::CVImageBufferRef, os_types::rc::Id, sc_stream_frame_info::SCFrameStatus,
 };
 
 use crate::cv_pixel_buffer::CVPixelBuffer;
@@ -10,6 +10,7 @@ pub struct CMSampleBuffer {
     pub sys_ref: Id<CMSampleBufferRef>,
     pub image_buf_ref: Option<Id<CVImageBufferRef>>,
     pub pixel_buffer: Option<CVPixelBuffer>,
+    pub audio_buffer_list: Option<Vec<CopiedAudioBuffer>>,
     pub frame_status: SCFrameStatus,
 }
 
@@ -17,11 +18,15 @@ impl CMSampleBuffer {
     pub fn new(sys_ref: Id<CMSampleBufferRef>) -> Self {
         let frame_status = sys_ref.get_frame_info().status();
         let image_buf_ref = sys_ref.get_image_buffer();
-        let pixel_buffer = image_buf_ref.as_ref().map(|i| CVPixelBuffer::new(i.as_pixel_buffer()));
+        let audio_buffer_list = sys_ref.get_av_audio_buffer_list();
+        let pixel_buffer = image_buf_ref
+            .as_ref()
+            .map(|i| CVPixelBuffer::new(i.as_pixel_buffer()));
         Self {
             sys_ref,
             image_buf_ref,
             pixel_buffer,
+            audio_buffer_list,
             frame_status,
         }
     }
