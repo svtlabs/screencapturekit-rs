e5e3becbf44dfe98f4b298800897f70fd4079970
1313 <mail@perjohansson.net>
2023-12-29 21:13:00 +0100

    CI ignore test

--- screencapturekit/src/sc_content_filter.rs
+++ screencapturekit/src/sc_content_filter.rs
@@ -83,6 +83,7 @@ mod tests {
 
     use super::InitParams::Display;
     use super::*;
+    #[cfg_attr(feature = "ci", ignore)]
     #[test]
     fn test_sc_filter() {
         let display = SCShareableContent::current().displays.pop().unwrap();
