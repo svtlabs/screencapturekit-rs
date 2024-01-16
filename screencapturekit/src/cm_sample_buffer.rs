dbcc7f091164e6050e13bcea3bfdbfc045805976
Per Johansson <mail@perjohansson.net>
2024-01-16 09:21:22 +0100

    Fix ID issue refcount (#31)
    * Fix ID issue refcount
    
    * Fix ID issue refcount

--- screencapturekit/src/cm_sample_buffer.rs
+++ screencapturekit/src/cm_sample_buffer.rs
@@ -1,6 +1,8 @@
 use screencapturekit_sys::{
-    cm_sample_buffer_ref::CMSampleBufferRef, cv_image_buffer_ref::CVImageBufferRef,
-    os_types::rc::Id, sc_stream_frame_info::SCFrameStatus,
+    cm_sample_buffer_ref::CMSampleBufferRef,
+    cv_image_buffer_ref::CVImageBufferRef,
+    os_types::rc::{Id, ShareId},
+    sc_stream_frame_info::SCFrameStatus,
 };
 
 use crate::cv_pixel_buffer::CVPixelBuffer;
@@ -8,7 +10,7 @@ use crate::cv_pixel_buffer::CVPixelBuffer;
 #[derive(Debug)]
 pub struct CMSampleBuffer {
     pub sys_ref: Id<CMSampleBufferRef>,
-    pub image_buf_ref: Option<Id<CVImageBufferRef>>,
+    pub image_buf_ref: Option<ShareId<CVImageBufferRef>>,
     pub pixel_buffer: Option<CVPixelBuffer>,
     pub frame_status: SCFrameStatus,
 }
@@ -17,11 +19,13 @@ impl CMSampleBuffer {
     pub fn new(sys_ref: Id<CMSampleBufferRef>) -> Self {
         let frame_status = sys_ref.get_frame_info().status();
         let image_buf_ref = sys_ref.get_image_buffer();
-        let pixel_buffer = image_buf_ref.as_ref().map(|i| CVPixelBuffer::new(i.as_pixel_buffer()));
+        let pixel_buffer = image_buf_ref
+            .as_ref()
+            .map(|i| CVPixelBuffer::new(i.clone().as_pixel_buffer()));
         Self {
             sys_ref,
-            image_buf_ref,
             pixel_buffer,
+            image_buf_ref,
             frame_status,
         }
     }
