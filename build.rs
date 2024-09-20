53bdf639c8ac801e4fac971a3fb09afe0f0ebff9
Per Johansson <mail@perjohansson.net>
2024-09-20 15:39:52 +0200

    refactor(reps): remove linking on CoreMedia and CoreVideo

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
 fn main() {
     println!("cargo:rustc-link-lib=framework=CoreFoundation");
-    println!("cargo:rustc-link-lib=framework=CoreMedia");
-    println!("cargo:rustc-link-lib=framework=CoreVideo");
     println!("cargo:rustc-link-lib=framework=Foundation");
     println!("cargo:rustc-link-lib=framework=CoreGraphics");
     println!("cargo:rustc-link-lib=framework=CoreImage");
